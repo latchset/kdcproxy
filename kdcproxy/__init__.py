5edbbeb7f950ed9db60b11f0fdce1ec96194f761
Nathaniel McCallum <npmccallum@redhat.com>
2015-08-03 16:52:14 -0400

    Properly handle missing Content-Length

--- kdcproxy/__init__.py
+++ kdcproxy/__init__.py
@@ -178,10 +178,13 @@ class Application:
                 raise HTTPException(405, "Method not allowed (%s)." % method)
 
             # Parse the request
+            length = -1
             try:
                 length = int(env["CONTENT_LENGTH"])
-            except AttributeError:
-                raise HTTPException(411, "Length required.")
+            except KeyError:
+                pass
+            except ValueError:
+                pass
             if length < 0:
                 raise HTTPException(411, "Length required.")
             if length > self.MAX_LENGTH:
